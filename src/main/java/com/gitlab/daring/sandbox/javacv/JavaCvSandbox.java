1363e2d7add2222a6022fc214544f935238758d5
daring <danila.daring@gmail.com>
2017-01-14 14:28:27 +0300

    issue #1 - refactoring

--- src/main/java/com/gitlab/daring/sandbox/javacv/JavaCvSandbox.java
+++ src/main/java/com/gitlab/daring/sandbox/javacv/JavaCvSandbox.java
@@ -1,19 +1,23 @@
 package com.gitlab.daring.sandbox.javacv;
 
-import org.bytedeco.javacpp.FloatPointer;
-import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.OpenCVFrameConverter;
+import static com.google.common.base.MoreObjects.firstNonNull;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-import static org.bytedeco.javacpp.opencv_core.CV_32F;
+import static org.bytedeco.javacpp.opencv_core.Mat;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
-import static org.bytedeco.javacpp.opencv_imgproc.filter2D;
 
 public class JavaCvSandbox {
 
 	public static void main(String[] args) {
-		Mat m1 = imread("data/boldt.jpg"); // IMREAD_GRAYSCALE
-		show(m1, "original");
+		loadAndShow("data/boldt.jpg", null);
+		loadAndShow("data/rain.jpg", null);
+	}
+
+	private static Mat loadAndShow(String file, String title) {
+		Mat m = imread(file);// IMREAD_GRAYSCALE
+		show(m, firstNonNull(title, file));
+		return m;
 	}
 
 	private static void show(Mat m, String title) {
