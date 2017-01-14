d748e843fec4058254141f4cc96b39264a2a02fe
daring <danila.daring@gmail.com>
2017-01-14 12:33:25 +0300

    issue #1 - refactoring

--- src/main/java/com/gitlab/daring/sandbox/javacv/JavaCvSandbox.java
+++ src/main/java/com/gitlab/daring/sandbox/javacv/JavaCvSandbox.java
@@ -1,19 +1,25 @@
 package com.gitlab.daring.sandbox.javacv;
 
-import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.FloatPointer;
+import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-import static org.bytedeco.javacpp.opencv_imgcodecs.IMREAD_GRAYSCALE;
+import static org.bytedeco.javacpp.opencv_core.CV_32F;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+import static org.bytedeco.javacpp.opencv_imgproc.filter2D;
 
 public class JavaCvSandbox {
 
 	public static void main(String[] args) {
-		opencv_core.Mat image = imread("data/boldt.jpg", IMREAD_GRAYSCALE);
-		CanvasFrame frame = new CanvasFrame("My Image", 1);
+		Mat m1 = imread("data/boldt.jpg"); // IMREAD_GRAYSCALE
+		show(m1, "original");
+	}
+
+	private static void show(Mat m, String title) {
+		CanvasFrame frame = new CanvasFrame(title, 1);
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		frame.showImage(new OpenCVFrameConverter.ToMat().convert(image));
+		frame.showImage(new OpenCVFrameConverter.ToMat().convert(m));
 	}
 
 }
