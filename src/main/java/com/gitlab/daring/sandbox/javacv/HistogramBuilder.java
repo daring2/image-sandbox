cc2469eb9c2a7b2acbeb6d9f03749523423682ca
daring <danila.daring@gmail.com>
2017-01-16 15:04:20 +0300

    issue #2 - HistogramBuilder - add  buildImage method

--- src/main/java/com/gitlab/daring/sandbox/javacv/HistogramBuilder.java
+++ src/main/java/com/gitlab/daring/sandbox/javacv/HistogramBuilder.java
@@ -1,16 +1,36 @@
 package com.gitlab.daring.sandbox.javacv;
 
+import com.google.common.primitives.Floats;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.MatVector;
+import org.bytedeco.javacpp.opencv_core.Point;
+import org.bytedeco.javacpp.opencv_core.Scalar;
+import static com.gitlab.daring.sandbox.javacv.ConvertUtils.imageToFloatArray;
+import static org.bytedeco.javacpp.opencv_core.CV_8U;
 import static org.bytedeco.javacpp.opencv_imgproc.calcHist;
+import static org.bytedeco.javacpp.opencv_imgproc.line;
 
 class HistogramBuilder {
+
+	int size = 256;
 
 	Mat build(Mat m) {
 		MatVector mv = new MatVector(1).put(0, m);
 		Mat h = new Mat();
-		calcHist(mv, new int[] {0}, new Mat(), h, new int[] {256}, new float[] {0, 255});
+		calcHist(mv, new int[] {0}, new Mat(), h, new int[] {size}, new float[] {0, 255});
 		return h;
+	}
+
+	Mat buildImage(Mat m) {
+		Mat h = build(m);
+		float[] hvs = imageToFloatArray(h);
+		double scale = 0.9 * size / Floats.max(hvs);
+		Mat r = new Mat(size, size, CV_8U, Scalar.WHITE);
+		for (int i = 0; i < hvs.length; i++) {
+			int v = (int) (hvs[i] * scale);
+			line(r, new Point(i, size), new Point(i, size - v), Scalar.BLACK);
+		}
+		return r;
 	}
 
 }
--- src/main/java/com/gitlab/daring/sandbox/javacv/JavaCvSandbox.java
+++ src/main/java/com/gitlab/daring/sandbox/javacv/JavaCvSandbox.java
@@ -1,12 +1,16 @@
 package com.gitlab.daring.sandbox.javacv;
 
+import org.bytedeco.javacpp.opencv_core.Mat;
 import static com.gitlab.daring.sandbox.javacv.JavaCvUtils.loadAndShow;
+import static com.gitlab.daring.sandbox.javacv.JavaCvUtils.show;
+import static org.bytedeco.javacpp.opencv_imgcodecs.IMREAD_GRAYSCALE;
 
 public class JavaCvSandbox {
 
 	public static void main(String[] args) {
-		loadAndShow("data/boldt.jpg");
-		loadAndShow("data/rain.jpg");
+		Mat m1 = loadAndShow("data/group.jpg", IMREAD_GRAYSCALE, null);
+		Mat h1 = new HistogramBuilder().buildImage(m1);
+		show(h1, "histogram");
 	}
 
 }
