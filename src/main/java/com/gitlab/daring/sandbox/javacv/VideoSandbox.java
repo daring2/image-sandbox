9a10cd9fa3899f2c4617fefacffcfffe19aa2888
daring <danila.daring@gmail.com>
2017-01-21 20:53:49 +0300

    issue #2 - extend VideoSandbox

--- src/main/java/com/gitlab/daring/sandbox/javacv/VideoSandbox.java
+++ src/main/java/com/gitlab/daring/sandbox/javacv/VideoSandbox.java
@@ -1,14 +1,18 @@
 package com.gitlab.daring.sandbox.javacv;
 
 import org.bytedeco.javacpp.opencv_core.Mat;
-import org.bytedeco.javacpp.opencv_videoio.*;
+import org.bytedeco.javacpp.opencv_core.Size;
+import org.bytedeco.javacpp.opencv_videoio.VideoCapture;
+import org.bytedeco.javacpp.opencv_videoio.VideoWriter;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 
+import static com.gitlab.daring.sandbox.javacv.VideoUtils.XVID;
+import static com.gitlab.daring.sandbox.javacv.VideoUtils.getVideoSize;
 import static java.lang.Thread.sleep;
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
-import static org.bytedeco.javacpp.opencv_videoio.*;
+import static org.bytedeco.javacpp.opencv_videoio.CAP_PROP_FPS;
 
 public class VideoSandbox {
 
@@ -22,19 +26,27 @@ public class VideoSandbox {
 		long delay = fps > 0 ? Math.round(1000 / fps) : 100;
 		Mat in = new Mat();
 		Mat out = new Mat();
+		VideoWriter writer = new VideoWriter();
+		int codec = XVID; // (int) cap.get(CV_CAP_PROP_FOURCC);
+		writer.open("data/result.avi", codec, 1000.0 / delay, getVideoSize(cap), true);
 		while (cap.read(in)) {
 			frame1.showImage(conv.convert(in));
 			calcCanny(in, out);
+			writer.write(out);
 			frame2.showImage(conv.convert(out));
 			sleep(delay);
+			if (!frame1.isVisible() || !frame2.isVisible()) break;
 		}
 		cap.release();
+		writer.release();
+		System.exit(0);
 	}
 
 	private static CanvasFrame newFrame(VideoCapture cap, String title) {
-		CanvasFrame frame = new CanvasFrame("title", 1);
-		frame.setCanvasSize((int) cap.get(CAP_PROP_FRAME_WIDTH), (int) cap.get(CAP_PROP_FRAME_HEIGHT));
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		CanvasFrame frame = new CanvasFrame(title, 1);
+		Size size = getVideoSize(cap);
+		frame.setCanvasSize(size.width(), size.height());
+		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		return frame;
 	}
 
