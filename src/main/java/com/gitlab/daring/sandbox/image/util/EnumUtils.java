bdb77ea394a9249f566aa2573ac95b956436a423
daring <danila.daring@gmail.com>
2017-01-27 23:51:01 +0300

    issue #3 - refactoring

--- src/main/java/com/gitlab/daring/sandbox/image/util/EnumUtils.java
+++ src/main/java/com/gitlab/daring/sandbox/image/util/EnumUtils.java
@@ -1,6 +1,8 @@
 package com.gitlab.daring.sandbox.image.util;
 
 
+import com.typesafe.config.Config;
+
 public class EnumUtils {
 
 	public static <T> T findEnum(T[] vs, String name) {
@@ -10,6 +12,10 @@ public class EnumUtils {
 			if (vn.startsWith(ln)) return v;
 		}
 		throw new IllegalArgumentException("name=" + name);
+	}
+
+	public static <T> T findEnum(Config c, String path, T[] vs) {
+		return findEnum(vs, c.getString(path));
 	}
 
 	public static int findEnumIndex(Enum[] vs, String name) {
