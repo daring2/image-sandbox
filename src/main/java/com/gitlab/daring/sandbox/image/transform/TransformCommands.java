f2b17aa82112ed6d1ef2250c79021bc5d587d950
daring <danila.daring@gmail.com>
2017-01-29 16:48:15 +0300

    issue #3 - add adaptiveThreshold command

--- src/main/java/com/gitlab/daring/sandbox/image/transform/TransformCommands.java
+++ src/main/java/com/gitlab/daring/sandbox/image/transform/TransformCommands.java
@@ -12,38 +12,30 @@ import static org.bytedeco.javacpp.opencv_imgproc.*;
 public class TransformCommands {
 
 	public static void register(CommandRegistry r) {
-		TransformCommands p = new TransformCommands();
+		TransformCommands i = new TransformCommands();
 		r.register("convert", ConvertCommand::new);
-		r.register("equalizeHist", p::newEqualizeHistCmd);
-		r.register("threshold", p::newThresholdCmd);
-		r.register("morphology", p::newMorphologyCmd);
-		r.register("canny", p::newCannyCmd);
+		r.register("equalizeHist", i::newEqualizeHistCmd);
+		r.register("morphology", i::newMorphologyCmd);
+		r.register("canny", i::newCannyCmd);
+		ThresholdCommands.register(r);
 	}
 
-	public Command newEqualizeHistCmd(String[] params) {
+	public Command newEqualizeHistCmd(String[] ps) {
 		return newCommand(m -> equalizeHist(m, m));
 	}
 
-	public Command newThresholdCmd(String[] params) {
-		double th1 = parseDouble(params[0]);
-		double maxValue = parseDouble(params[1]);
-		int type = findEnumIndex(ThresholdType.values(), params[2]);
-		return newCommand(m -> threshold(m, m, th1, maxValue, type));
-	}
 
-	enum ThresholdType { Bin, BinInv, Trunc, ToZero, ToZeroInv }
-
-	public Command newMorphologyCmd(String[] params) {
-		int op = findEnumIndex(MorphOperation.values(), params[0]);
+	public Command newMorphologyCmd(String[] ps) {
+		int op = findEnumIndex(MorphOperation.values(), ps[0]);
 		Mat kernel = new Mat();
 		return newCommand(m -> morphologyEx(m, m, op, kernel));
 	}
 
 	enum MorphOperation { Erode, Dilate, Open, Close, Gradient, TopHat, BlackHat, HitMiss }
 
-	public Command newCannyCmd(String[] params) {
-		double th1 = parseDouble(params[0]);
-		double th2 = parseDouble(params[1]);
+	public Command newCannyCmd(String[] ps) {
+		double th1 = parseDouble(ps[0]);
+		double th2 = parseDouble(ps[1]);
 		return newCommand(m -> Canny(m, m, th1, th2));
 	}
 
