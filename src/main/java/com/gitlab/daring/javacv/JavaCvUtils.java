029964bd930eca39f18d16a0cefe52353441d4f3
daring <danila.daring@gmail.com>
2017-01-30 00:01:10 +0300

    issue #3 - remove unused code

--- src/main/java/com/gitlab/daring/javacv/JavaCvUtils.java
+++ src/main/java/com/gitlab/daring/javacv/JavaCvUtils.java
@@ -4,18 +4,9 @@ import org.bytedeco.javacpp.IntPointer;
 import org.bytedeco.javacpp.opencv_core.Mat;
 
 import static com.gitlab.daring.image.util.ImageUtils.buildMat;
-import static com.gitlab.daring.image.util.ImageUtils.showMat;
-import static com.google.common.base.MoreObjects.firstNonNull;
 import static org.bytedeco.javacpp.opencv_core.normalize;
-import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
 
 class JavaCvUtils {
-
-	static Mat loadAndShow(String file, int flags, String title) {
-		Mat m = imread(file, flags);
-		showMat(m, firstNonNull(title, file));
-		return m;
-	}
 
 	static Mat normalizeImage(Mat m) {
 		return buildMat(r -> normalize(m, r));
