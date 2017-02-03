e90e792e748ecfa3920c5ce9e1b05fa459659a1c
daring <danila.daring@gmail.com>
2017-02-04 00:36:31 +0300

    issue #4 - refactoring

--- src/main/java/com/gitlab/daring/image/command/transform/ThresholdCommands.java
+++ src/main/java/com/gitlab/daring/image/command/transform/ThresholdCommands.java
@@ -31,9 +31,11 @@ public class ThresholdCommands {
 		DoubleParam mv = c.doubleParam(0, "0-255");
 		EnumParam<AdaptiveMethod> method = c.enumParam(AdaptiveMethod.class, 1);
 		EnumParam<ThresholdType> type = c.enumParam(ThresholdType.class, 2);
-		IntParam blockSize = c.intParam(3, "0-30");
+		IntParam bs = c.intParam(3, "1-50");
 		IntParam cf = c.intParam(4, "0-100");
-		return c.withFunc(m -> adaptiveThreshold(m, m, mv.v, method.vi(), type.vi(), blockSize.v, cf.v));
+		return c.withFunc(m ->
+			adaptiveThreshold(m, m, mv.v, method.vi(), type.vi(), bs.v * 2 + 1, cf.v)
+		);
 	}
 
 	enum ThresholdType { Bin, BinInv, Trunc, ToZero, ToZeroInv }
