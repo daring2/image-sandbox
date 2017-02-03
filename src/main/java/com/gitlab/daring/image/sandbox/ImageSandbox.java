6acbc1abbcd1bcfbeabbf5eb555133b9bcf2156d
daring <danila.daring@gmail.com>
2017-02-03 14:33:43 +0300

    issue #4 - refactoring

--- src/main/java/com/gitlab/daring/image/sandbox/ImageSandbox.java
+++ src/main/java/com/gitlab/daring/image/sandbox/ImageSandbox.java
@@ -1,44 +1,37 @@
 package com.gitlab.daring.image.sandbox;
 
 import com.gitlab.daring.image.command.CommandEnv;
-import com.gitlab.daring.image.command.CompositeCommand;
 import com.gitlab.daring.image.common.BaseComponent;
 import com.typesafe.config.Config;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.gitlab.daring.image.command.CommandRegistry.parseCmdScript;
 import static com.gitlab.daring.image.config.ConfigUtils.configFromMap;
 import static com.gitlab.daring.image.config.ConfigUtils.saveDiffConfig;
-import static com.gitlab.daring.image.util.CommonUtils.closeQuietly;
 
 public class ImageSandbox extends BaseComponent {
 
 	static final String ConfigPath = "gmv.ImageSandbox";
 
-	final MainPanel mainPanel = new MainPanel(this);
+	final MainPanel mp = new MainPanel(this);
 	final CommandEnv cmdEnv = new CommandEnv();
-
-	String script;
-	CompositeCommand scriptCmd;
 
 	public ImageSandbox() {
 		super(ConfigPath);
-		mainPanel.applyEvent.onFire(this::saveConfig);
-		mainPanel.showFrame();
+		mp.applyEvent.onFire(this::apply);
+		mp.showFrame();
+		mp.setScript(config.getString("script"));
 	}
 
-	void setScript(String script) {
-		closeQuietly(scriptCmd);
-		this.script = script;
-		scriptCmd = parseCmdScript(script);
-		scriptCmd.execute(cmdEnv);
+	void apply() {
+		mp.getScriptCommand().execute(cmdEnv);
+		saveConfig();
 	}
 
 	void saveConfig() {
 		Map<String, Object> m = new HashMap<>();
-		m.put("script", script);
+		m.put("script", mp.getScript());
 		Config c = configFromMap(m).atPath(ConfigPath);
 		saveDiffConfig(c, "conf/application.conf");
 	}
--- src/main/java/com/gitlab/daring/image/sandbox/MainPanel.java
+++ src/main/java/com/gitlab/daring/image/sandbox/MainPanel.java
@@ -1,36 +1,14 @@
 package com.gitlab.daring.image.sandbox;
 
-import com.gitlab.daring.image.event.VoidEvent;
-import com.gitlab.daring.image.swing.BaseAction;
+import com.gitlab.daring.image.command.CommandScriptPanel;
 import com.gitlab.daring.image.swing.BaseFrame;
-import net.miginfocom.swing.MigLayout;
 
-import javax.swing.*;
-
-class MainPanel extends JPanel {
+class MainPanel extends CommandScriptPanel {
 
 	final ImageSandbox sb;
-	final VoidEvent applyEvent = new VoidEvent();
 
 	MainPanel(ImageSandbox sb) {
 		this.sb = sb;
-		setLayout(new MigLayout("fill, wrap 2", "[right][grow,fill]", "[center]"));
-		createScriptField();
-		createButtons();
-	}
-
-	void createScriptField() {
-		String script = sb.config.getString("script");
-		JTextArea field = new JTextArea(script, 5, 10);
-		add(new JLabel("Скрипт"), "left, span 2");
-		add(new JScrollPane(field), "h 1000, grow, span 2");
-		applyEvent.onFire(() -> sb.setScript(field.getText()));
-	}
-
-	void createButtons() {
-		BaseAction act = new BaseAction("Применить", e -> applyEvent.fire() );
-		act.register(this, "control S");
-		add(new JButton(act), "left, span 2");
 	}
 
 	void showFrame() {
