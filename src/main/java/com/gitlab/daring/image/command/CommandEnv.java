3a40944b7c5e22d0d767b59e8fe07535d62da53a
daring <danila.daring@gmail.com>
2017-02-04 22:59:23 +0300

    issue #4 - refactoring

--- src/main/java/com/gitlab/daring/image/command/CommandEnv.java
+++ src/main/java/com/gitlab/daring/image/command/CommandEnv.java
@@ -10,4 +10,12 @@ public class CommandEnv {
 	public Mat mat = new Mat();
 	public Map<String, Mat> mats = new HashMap<>();
 
+	public Mat getMat(String key) {
+		return key.isEmpty() ? mat : mats.get(key);
+	}
+
+	public void setMat(String key, Mat m) {
+		if (key.isEmpty()) mat = m; else mats.put(key, m);
+	}
+
 }
--- src/main/java/com/gitlab/daring/image/command/EnvCommands.java
+++ src/main/java/com/gitlab/daring/image/command/EnvCommands.java
@@ -2,7 +2,6 @@ package com.gitlab.daring.image.command;
 
 import com.gitlab.daring.image.command.parameter.EnumParam;
 
-import static com.gitlab.daring.image.command.CommandUtils.newCommand;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imwrite;
 
@@ -20,18 +19,18 @@ public class EnvCommands {
 	public Command newReadCommand(String... ps) {
 		SimpleCommand c = new SimpleCommand(ps);
 		EnumParam<ReadFlag> flags = c.enumParam(ReadFlag.class, 1);
-		c.func = env -> env.mat = imread(ps[0], flags.vi());
+		c.func = env -> env.setMat(ps[1], imread(ps[0], flags.vi()));
 		return c;
 	}
 
 	enum ReadFlag { Grey, Color }
 
 	public Command newWriteCommand(String... ps) {
-		return newCommand(m -> imwrite(ps[0], m));
+		return env -> imwrite(ps[0], env.getMat(ps[1]));
 	}
 
 	public Command newGetCommand(String... ps) {
-		return env -> env.mats.get(ps[0]).copyTo(env.mat);
+		return env -> env.getMat(ps[0]).copyTo(env.mat);
 	}
 
 	public Command newPutCommand(String... ps) {
