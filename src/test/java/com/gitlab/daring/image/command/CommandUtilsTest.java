170dc8de162ac4c4c35e900793bf4ffbb67070d1
daring <danila.daring@gmail.com>
2017-02-09 21:03:45 +0300

    issue #4 - add SIFT and SURF commands

--- src/test/java/com/gitlab/daring/image/command/CommandUtilsTest.java
+++ src/test/java/com/gitlab/daring/image/command/CommandUtilsTest.java
@@ -8,6 +8,7 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertEquals;
 
+@SuppressWarnings("ArraysAsListWithZeroOrOneArgument")
 public class CommandUtilsTest {
 
 	@Test
@@ -18,8 +19,22 @@ public class CommandUtilsTest {
 		assertEquals(asList("p1", "d2", "d3"), parseArgs("p1", "d1", "d2", "d3"));
 	}
 
-	List<String> parseArgs(String paramStr, String... defParams) {
-		return asList(CommandUtils.parseArgs(paramStr, asList(defParams)));
+	List<String> parseArgs(String argStr, String... defArgs) {
+		return asList(CommandUtils.parseArgs(argStr, asList(defArgs)));
+	}
+
+	@Test
+	public void testBuildArgs() throws Exception {
+		assertEquals(emptyList(), buildArgs(emptyList(), emptyList()));
+		List<String> args = asList("p1", "p2");
+		List<String> defArgs = asList("d1", "d2", "d3");
+		assertEquals(args, buildArgs(args, emptyList()));
+		assertEquals(defArgs, buildArgs(emptyList(), defArgs));
+		assertEquals(asList("p1", "d2", "d3"), buildArgs(asList("p1"), defArgs));
+	}
+
+	List<String> buildArgs(List<String> args, List<String> defArgs) {
+		return asList(CommandUtils.buildArgs(args, defArgs));
 	}
 
 }
