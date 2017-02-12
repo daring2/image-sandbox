34c1d826eec2844c7c7cb4b42e0645b6691ec142
daring <danila.daring@gmail.com>
2017-02-12 17:15:03 +0300

    issue #4 - refactoring

--- src/main/java/com/gitlab/daring/image/command/structure/DrawContoursCommand.java
+++ src/main/java/com/gitlab/daring/image/command/structure/DrawContoursCommand.java
@@ -9,8 +9,10 @@ import org.bytedeco.javacpp.opencv_core.Point;
 import org.bytedeco.javacpp.opencv_core.Scalar;
 
 import static com.gitlab.daring.image.util.CollectionUtils.mapList;
+import static com.gitlab.daring.image.util.ImageUtils.newScalarMat;
 import static com.gitlab.daring.image.util.OpencvConverters.toOpencv;
 import static org.bytedeco.javacpp.opencv_core.LINE_8;
+import static org.bytedeco.javacpp.opencv_core.min;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_FILLED;
 import static org.bytedeco.javacpp.opencv_imgproc.drawContours;
 
@@ -19,6 +21,7 @@ public class DrawContoursCommand extends BaseCommand {
 	final IntParam index = intParam(0, 0, "0-10");
 	final IntParam thickness = intParam(1, 1, "0-10");
 	final int maxLevel = Integer.MAX_VALUE;
+
 	final Mat h = new Mat();
 	final Point offset = new Point();
 
@@ -28,7 +31,8 @@ public class DrawContoursCommand extends BaseCommand {
 
 	@Override
 	public void execute(CommandEnv env) {
-		env.mat = new Mat(env.mat.size(), env.mat.type(), Scalar.BLACK);
+		min(env.mat, newScalarMat(0), env.mat);
+		if (index.v > env.contours.size()) return;
 		MatVector cs = toOpencv(mapList(env.contours, c -> c.mat));
 		int th = thickness.posVal(CV_FILLED);
 		drawContours(env.mat, cs, index.v - 1, Scalar.WHITE, th, LINE_8, h, maxLevel, offset);
