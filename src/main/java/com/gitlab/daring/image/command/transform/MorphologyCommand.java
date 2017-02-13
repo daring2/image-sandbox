56d833b42812581bce80fb1b1d223f32cfd4daf8
daring <danila.daring@gmail.com>
2017-02-13 23:38:54 +0300

    issue #4 - MorphologyCommand - add iterCount param

--- src/main/java/com/gitlab/daring/image/command/transform/MorphologyCommand.java
+++ src/main/java/com/gitlab/daring/image/command/transform/MorphologyCommand.java
@@ -3,13 +3,15 @@ package com.gitlab.daring.image.command.transform;
 import com.gitlab.daring.image.command.BaseCommand;
 import com.gitlab.daring.image.command.CommandEnv;
 import com.gitlab.daring.image.command.parameter.EnumParam;
+import com.gitlab.daring.image.command.parameter.IntParam;
 import org.bytedeco.javacpp.opencv_core.Mat;
 
 import static org.bytedeco.javacpp.opencv_imgproc.morphologyEx;
 
 public class MorphologyCommand extends BaseCommand {
 
-	final EnumParam<Operation> operation = enumParam(Operation.class, 0, null);
+	final EnumParam<Operation> operation = enumParam(Operation.class, null);
+	final IntParam iterCount = intParam(1, "0-10");
 	final Mat kernel = new Mat();
 
 	public MorphologyCommand(String... args) {
@@ -18,7 +20,8 @@ public class MorphologyCommand extends BaseCommand {
 
 	@Override
 	public void execute(CommandEnv env) {
-		morphologyEx(env.mat, env.mat, operation.vi(), kernel);
+		for (int i = 0; i < iterCount.v; i++)
+			morphologyEx(env.mat, env.mat, operation.vi(), kernel);
 	}
 
 	enum Operation { Erode, Dilate, Open, Close, Gradient, TopHat, BlackHat, HitMiss }
