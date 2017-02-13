31fb1c20991ac1f733f4964c7b818cd6b18173c4
daring <danila.daring@gmail.com>
2017-02-13 18:49:41 +0300

    issue #4 - refactoring

--- src/main/java/com/gitlab/daring/image/command/transform/NormalizeCommand.java
+++ src/main/java/com/gitlab/daring/image/command/transform/NormalizeCommand.java
@@ -6,14 +6,13 @@ import com.gitlab.daring.image.command.parameter.EnumParam;
 import com.gitlab.daring.image.command.parameter.IntParam;
 import org.bytedeco.javacpp.opencv_core.Mat;
 
-import static org.bytedeco.javacpp.opencv_core.NORM_MINMAX;
 import static org.bytedeco.javacpp.opencv_core.normalize;
 
 public class NormalizeCommand extends BaseCommand {
 
 	final IntParam alpha = intParam(0, "0-255");
 	final IntParam beta = intParam(255, "0-255");
-	final EnumParam<NormType> normType = enumParam(NormType.class, NormType.MIN_MAX);
+	final EnumParam<NormType> normType = enumParam(NormType.class, NormType.MinMax);
 
 	final Mat mask = new Mat();
 
@@ -23,13 +22,20 @@ public class NormalizeCommand extends BaseCommand {
 
 	@Override
 	public void execute(CommandEnv env) {
-		int nt = normType.vi();
-		if (nt == 0) nt = NORM_MINMAX;
 		Mat rm = new Mat();
+		int nt = normType.v.code;
 		normalize(env.mat, rm, alpha.v, beta.v, nt, -1, mask);
 		env.mat = rm;
 	}
 
-	enum  NormType { MIN_MAX, INF, L1, L2 }
+	public enum NormType {
+
+		MinMax(32), Inf(1), L19(2), L2(4);
+
+		public final int code;
+
+		NormType(int code) { this.code = code; }
+
+	}
 
 }
