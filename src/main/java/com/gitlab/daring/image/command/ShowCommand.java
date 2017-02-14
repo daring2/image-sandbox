0c7fc80c5b0dbaedb847fd294be6c85353b49be6
daring <danila.daring@gmail.com>
2017-02-14 17:12:55 +0300

    issue #4 - refactoring

--- src/main/java/com/gitlab/daring/image/command/ShowCommand.java
+++ src/main/java/com/gitlab/daring/image/command/ShowCommand.java
@@ -3,10 +3,7 @@ package com.gitlab.daring.image.command;
 import com.gitlab.daring.image.component.BaseCanvasFrame;
 import org.bytedeco.javacv.OpenCVFrameConverter.ToMat;
 
-import java.awt.*;
-
 import static com.gitlab.daring.image.swing.SwingUtils.runInEdt;
-import static com.gitlab.daring.image.util.GeometryUtils.scaleToMax;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 import static javax.swing.WindowConstants.HIDE_ON_CLOSE;
 
@@ -15,7 +12,6 @@ public class ShowCommand extends BaseCommand {
 	final String title = args[0];
 	final BaseCanvasFrame frame = new BaseCanvasFrame(title);
 	final ToMat converter = new ToMat();
-	final Dimension maxSize = new Dimension(1024, 768);
 
 	public ShowCommand(String... params) {
 		super(params);
@@ -30,18 +26,9 @@ public class ShowCommand extends BaseCommand {
 
 	@Override
 	public void execute(CommandEnv env) {
-		runInEdt(() -> {
-			frame.showImage(converter.convert(env.mat));
-			if (!frame.isVisible()) {
-				resize();
-				frame.setVisible(true);
-			}
-		});
-	}
-
-	void resize() {
-		Dimension d = scaleToMax(frame.getCanvasSize(), maxSize);
-		frame.setCanvasSize(d.width, d.height);
+		runInEdt(() ->
+			frame.showImage(converter.convert(env.mat))
+		);
 	}
 
 	@Override
--- src/main/java/com/gitlab/daring/image/component/BaseCanvasFrame.java
+++ src/main/java/com/gitlab/daring/image/component/BaseCanvasFrame.java
@@ -4,16 +4,30 @@ import org.bytedeco.javacv.CanvasFrame;
 
 import java.awt.*;
 
+import static com.gitlab.daring.image.util.GeometryUtils.scaleToMax;
+
 public class BaseCanvasFrame extends CanvasFrame {
+
+	final Dimension maxSize = new Dimension(1024, 768);
 
 	public BaseCanvasFrame(String title) {
 		super(title, 1);
+		setVisible(false);
 	}
 
 	@Override
 	public void showImage(Image image) {
 		if (!isVisible()) needInitialResize = true;
 		super.showImage(image);
+		if (!isVisible()) {
+			resize();
+			setVisible(true);
+		}
+	}
+
+	public void resize() {
+		Dimension d = scaleToMax(getCanvasSize(), maxSize);
+		setCanvasSize(d.width, d.height);
 	}
 	
 }
