2b55d30f3420ac2cc2a45b8e9d83c3c24f06bd39
daring <danila.daring@gmail.com>
2017-03-02 18:15:25 +0300

    issue #4 - NumberParam - add pv method

--- src/main/java/com/gitlab/daring/image/command/combine/AddWeightedCommand.java
+++ src/main/java/com/gitlab/daring/image/command/combine/AddWeightedCommand.java
@@ -2,12 +2,18 @@ package com.gitlab.daring.image.command.combine;
 
 import com.gitlab.daring.image.command.BaseCommand;
 import com.gitlab.daring.image.command.CommandEnv;
+import com.gitlab.daring.image.command.parameter.DoubleParam;
+import com.gitlab.daring.image.command.parameter.StringParam;
 import org.bytedeco.javacpp.opencv_core.Mat;
 
-import static java.lang.Double.parseDouble;
 import static org.bytedeco.javacpp.opencv_core.addWeighted;
 
 public class AddWeightedCommand extends BaseCommand {
+
+	final StringParam key = stringParam("");
+	final DoubleParam f1 = doubleParam(50, "0-100");
+	final DoubleParam f2 = doubleParam(50, "0-100");
+	final DoubleParam f3 = doubleParam(0, "0-100");
 
 	public AddWeightedCommand(String... args) {
 		super(args);
@@ -15,11 +21,8 @@ public class AddWeightedCommand extends BaseCommand {
 
 	@Override
 	public void execute(CommandEnv env) {
-		Mat m2 = env.getMat(args[0]);
-		double f1 = parseDouble(args[1]);
-		double f2 = parseDouble(args[2]);
-		double f3 = parseDouble(args[3]);
-		addWeighted(env.mat, f1, m2, f2, f3, env.mat);
+		Mat m2 = env.getMat(key.v);
+		addWeighted(env.mat, f1.pv(), m2, f2.pv(), f3.pv(), env.mat);
 	}
 
 }
