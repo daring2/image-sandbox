4606a63350d1d647e91b5f900b44fb61c3eb517b
daring <danila.daring@gmail.com>
2017-03-05 22:02:48 +0300

    issue #6 - add broken test

---
+++ src/test/java/com/gitlab/daring/image/util/CommonUtilsTest.java
@@ -0,0 +1,21 @@
+package com.gitlab.daring.image.util;
+
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+import static com.gitlab.daring.image.util.CommonUtils.tryRun;
+import static org.junit.Assert.assertEquals;
+
+public class CommonUtilsTest {
+
+	@Test
+	public void testTryRun() {
+		Exception[] errRef = new Exception[1];
+		Consumer<Exception> errFunc = e -> errRef[0] = e;
+		Exception e1 = new Exception("e1");
+		tryRun(() -> { throw e1; }, errFunc);
+		assertEquals(null, errRef[0]);
+	}
+
+}
