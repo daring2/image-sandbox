c15d0b03569339dceb0c175ebfecd02ba782eab7
daring <danila.daring@gmail.com>
2017-03-16 22:20:47 +0300

    issue #10 - refactoring

---
+++ src/main/kotlin/com/gitlab/daring/image/event/ValueEvent.kt
@@ -0,0 +1,34 @@
+package com.gitlab.daring.image.event
+
+import java.util.concurrent.CopyOnWriteArrayList
+import java.util.function.Consumer
+
+open class ValueEvent<T> : Consumer<T> {
+
+    var enabled = true
+
+    private val listeners = CopyOnWriteArrayList<Consumer<T>>()
+
+    fun addListener(l: Consumer<T>) {
+        if (!listeners.contains(l))
+            listeners.add(l)
+    }
+
+    fun removeListener(l: Consumer<T>) {
+        listeners.remove(l)
+    }
+
+    fun removeListeners() {
+        listeners.clear()
+    }
+
+    fun fire(value: T) {
+        if (!enabled) return
+        listeners.forEach { c -> c.accept(value) }
+    }
+
+    override fun accept(v: T) {
+        fire(v)
+    }
+
+}
