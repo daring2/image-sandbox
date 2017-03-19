bb9bcd5a5625cf3b10c19e14b1b5503d1633a5b7
daring <danila.daring@gmail.com>
2017-03-19 16:22:27 +0300

    issue #11- refactoring

--- src/main/java/com/gitlab/daring/image/features/FeatureUtils.java
+++ src/main/java/com/gitlab/daring/image/features/FeatureUtils.java
@@ -17,6 +17,18 @@ public class FeatureUtils {
         return r;
     }
 
+    public static Mat buildMatchPoints(DMatchVector mv, KeyPointVector kps, boolean qps) {
+        int size = (int) mv.size();
+        Mat pm = new Mat(1, size, CV_32FC2);
+        FloatIndexer ind = pm.createIndexer();
+        for (int i = 0; i < size; i++) {
+            DMatch dm = mv.get(i);
+            KeyPoint kp = kps.get(qps ? dm.queryIdx() : dm.trainIdx());
+            Point2f pt = kp.pt(); ind.put(0, i, 0, pt.x()); ind.put(0, i, 1, pt.y());
+        }
+        return pm;
+    }
+
     public static Mat buildHomography(Feature2D fd, DescriptorMatcher dm, Mat m1, Mat m2) {
         KeyPointList ps1 = detectAndCompute(fd, m1);
         KeyPointList ps2 = detectAndCompute(fd, m2);
@@ -28,18 +40,6 @@ public class FeatureUtils {
             buildMatchPoints(mv, ps2.points, false),
             new Mat(), RANSAC, 3
         );
-    }
-
-    public static Mat buildMatchPoints(DMatchVector mv, KeyPointVector kps, boolean qps) {
-        int size = (int) mv.size();
-        Mat pm = new Mat(1, size, CV_32FC2);
-        FloatIndexer ind = pm.createIndexer();
-        for (int i = 0; i < size; i++) {
-            DMatch dm = mv.get(i);
-            KeyPoint kp = kps.get(qps ? dm.queryIdx() : dm.trainIdx());
-            Point2f pt = kp.pt(); ind.put(0, i, 0, pt.x()); ind.put(0, i, 1, pt.y());
-        }
-        return pm;
     }
 
     private FeatureUtils() {
