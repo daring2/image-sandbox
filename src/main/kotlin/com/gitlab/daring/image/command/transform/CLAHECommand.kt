bb93508210cc4f8d262bb72ff010becb2e672e92
daring <danila.daring@gmail.com>
2017-03-20 18:41:12 +0300

    issue #11 - add applyClahe command

---
+++ src/main/kotlin/com/gitlab/daring/image/command/transform/CLAHECommand.kt
@@ -0,0 +1,24 @@
+package com.gitlab.daring.image.command.transform
+
+import com.gitlab.daring.image.command.CommandEnv
+import com.gitlab.daring.image.command.KBaseCommand
+import org.bytedeco.javacpp.opencv_core.Mat
+import org.bytedeco.javacpp.opencv_core.Size
+import org.bytedeco.javacpp.opencv_imgproc.createCLAHE
+
+class CLAHECommand(vararg args: String): KBaseCommand(*args) {
+
+    val clipLimit = doubleParam(40.0, "0-255")
+    val tilesGridSize = intParam(8, "0-255")
+
+    val alg = createCLAHE()
+    val rm = Mat()
+
+    override fun execute(env: CommandEnv) {
+        alg.clipLimit = clipLimit.v
+        tilesGridSize.v.let { alg.tilesGridSize = Size(it, it) }
+        alg.apply(env.mat, rm)
+        env.mat = rm
+    }
+
+}
