157c75c94d794a7ff97b2ea426670b5066aef18a
daring <danila.daring@gmail.com>
2017-03-20 17:36:35 +0300

    issue #11 - refactoring

--- src/main/java/com/gitlab/daring/image/command/transform/TransformCommands.java
+++ src/main/java/com/gitlab/daring/image/command/transform/TransformCommands.java
@@ -2,13 +2,10 @@ package com.gitlab.daring.image.command.transform;
 
 import com.gitlab.daring.image.command.Command;
 import com.gitlab.daring.image.command.CommandRegistry;
-import com.gitlab.daring.image.command.SimpleCommand;
 import com.gitlab.daring.image.command.combine.BitwisePrevCommand;
-import com.gitlab.daring.image.command.parameter.IntParam;
 
 import static com.gitlab.daring.image.command.CommandUtils.newCommand;
-import static com.gitlab.daring.image.util.ImageUtils.smat;
-import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_core.bitwise_not;
 import static org.bytedeco.javacpp.opencv_imgproc.equalizeHist;
 
 public class TransformCommands {
@@ -17,7 +14,7 @@ public class TransformCommands {
         TransformCommands f = new TransformCommands();
         r.register("equalizeHist", f::equalizeHistCommand);
         r.register("bitwiseNot", f::bitwiseNotCommand);
-        r.register("keepRange", f::keepRange);
+        r.register("keepRange", KeepRangeCommand::new);
         r.register("convert", ConvertCommand::new);
         r.register("morphology", MorphologyCommand::new);
         r.register("bitwisePrev", BitwisePrevCommand::new);
@@ -33,15 +30,6 @@ public class TransformCommands {
 
     public Command bitwiseNotCommand(String... ps) {
         return newCommand(m -> bitwise_not(m, m));
-    }
-
-    public Command keepRange(String... ps) {
-        SimpleCommand c = new SimpleCommand(ps);
-        IntParam lb = c.intParam(0, "0-255");
-        IntParam ub = c.intParam(255, "0-255");
-        return c.withFunc((m, d) -> {
-            inRange(m, smat(lb.v), smat(ub.v), d); min(m, d, d);
-        });
     }
 
 }
---
+++ src/main/kotlin/com/gitlab/daring/image/command/transform/KeepRangeCommand.kt
@@ -0,0 +1,22 @@
+package com.gitlab.daring.image.command.transform
+
+import com.gitlab.daring.image.command.CommandEnv
+import com.gitlab.daring.image.command.KBaseCommand
+import com.gitlab.daring.image.util.ImageUtils.smat
+import org.bytedeco.javacpp.opencv_core.*
+
+class KeepRangeCommand(vararg args: String): KBaseCommand(*args) {
+
+    val lowerb = intParam(0, "0-255")
+    val upperb = intParam(255, "0-255")
+
+    val rm = Mat()
+
+    override fun execute(env: CommandEnv) {
+        val m = env.mat
+        inRange(m, smat(lowerb.v), smat(upperb.v), rm)
+        min(m, rm, rm)
+    }
+    
+}
+
