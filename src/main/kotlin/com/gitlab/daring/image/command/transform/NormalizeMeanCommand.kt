e92008ebda9a2a791ec16f4da82e0d0b8fa436a0
daring <danila.daring@gmail.com>
2017-03-21 12:10:00 +0300

    issue #11 - refactoring

--- src/main/kotlin/com/gitlab/daring/image/command/transform/NormalizeMeanCommand.kt
+++ src/main/kotlin/com/gitlab/daring/image/command/transform/NormalizeMeanCommand.kt
@@ -7,12 +7,11 @@ import org.bytedeco.javacpp.opencv_core.multiply
 
 class NormalizeMeanCommand(vararg args: String): KBaseCommand(*args) {
 
-    val matKey = stringParam("")
+    val meanValue = intParam(128, "0-255")
 
     override fun execute(env: CommandEnv) {
         val m1 = mean(env.mat)
-        val m2 = mean(env.getMat(matKey.v))
-        val f =  m2.get() / m1.get()
+        val f =  meanValue.v / m1.get()
         if (Math.abs(f - 1.0) > 0.001)
             env.mat = multiply(env.mat, f).asMat()
     }
