f189b863e72e4e34e3066c9627cb6bd80def4cb1
daring <danila.daring@gmail.com>
2017-04-04 19:05:10 +0300

    issue #10 - refactoring

---
+++ src/main/kotlin/com/gitlab/daring/image/command/transform/ScaleToSizeCommand.kt
@@ -0,0 +1,26 @@
+package com.gitlab.daring.image.command.transform
+
+import com.gitlab.daring.image.command.CommandEnv
+import com.gitlab.daring.image.command.KBaseCommand
+import org.bytedeco.javacpp.opencv_core.Mat
+import org.bytedeco.javacpp.opencv_core.Size
+import org.bytedeco.javacpp.opencv_imgproc.resize
+
+class ScaleToSizeCommand(vararg args: String) : KBaseCommand(*args) {
+
+    val matSize = intParam(0, "0-5000")
+    val minFactor = intParam(5, "0-100")
+    val method = enumParam<InterMethod>(InterMethod.Linear)
+
+    val rm = Mat()
+    val s0 = Size()
+
+    override fun execute(env: CommandEnv) {
+        val m = env.mat
+        val f = 1.0 * matSize.v / Math.max(m.rows(), m.cols())
+        if (f < minFactor.pv()) return
+        resize(m, rm, s0, f, f, method.vi())
+        env.mat = rm
+    }
+
+}
