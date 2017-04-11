35742cc5575935954243bafdb3a6fff2a9c3770a
daring <danila.daring@gmail.com>
2017-04-11 17:03:31 +0300

    issue #11 - ConvertCommand - add channel param

--- src/main/kotlin/com/gitlab/daring/image/command/transform/ConvertCommand.kt
+++ src/main/kotlin/com/gitlab/daring/image/command/transform/ConvertCommand.kt
@@ -4,20 +4,34 @@ import com.gitlab.daring.image.command.CommandEnv
 import com.gitlab.daring.image.command.KBaseCommand
 import com.gitlab.daring.image.command.transform.ConvertCommand.Target.Gray
 import com.gitlab.daring.image.opencv.gray
-import org.bytedeco.javacpp.opencv_core.Mat
+import com.gitlab.daring.image.opencv.size
+import com.gitlab.daring.image.opencv.type
+import org.bytedeco.javacpp.opencv_core.*
 import org.bytedeco.javacpp.opencv_imgproc.*
 
 internal class ConvertCommand(vararg params: String) : KBaseCommand(*params) {
 
-    val target = enumParam(Target.Gray)
+    val target = enumParam(Gray)
+    val channel = enumParam(Channel.None)
+    val rm = Mat()
 
     override fun execute(env: CommandEnv) {
-        if (env.mat.gray != (target.v == Gray)) {
-            val code = if (target.v == Gray) COLOR_BGR2GRAY else COLOR_GRAY2BGR
-            env.mat = Mat().apply { cvtColor(env.mat, this, code) }
+        if (env.mat.gray == (target.v == Gray)) return
+        val m = env.mat
+        val c = channel.v
+        if (target.v == Gray) {
+            cvtColor(m, rm, COLOR_BGR2GRAY)
+        } else if (c == Channel.None) {
+            cvtColor(m, rm, COLOR_GRAY2BGR)
+        } else {
+            val cms = Array(3, { Mat(m.size, m.type, Scalar.BLACK)})
+            m.copyTo(cms[c.ordinal])
+            merge(MatVector(*cms), rm)
         }
+        env.mat = rm
     }
 
     enum class Target { Gray, Color }
+    enum class Channel { Green, Blue, Red, None }
 
 }
