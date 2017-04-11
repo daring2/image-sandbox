6370b9603698e67a1ab2b1c30d158b27b5ff4ba3
daring <danila.daring@gmail.com>
2017-04-11 17:05:34 +0300

    issue #11 - introduce CombineCommand

---
+++ src/main/kotlin/com/gitlab/daring/image/command/combine/CombineCommand.kt
@@ -0,0 +1,23 @@
+package com.gitlab.daring.image.command.combine
+
+import com.gitlab.daring.image.command.CommandEnv
+import com.gitlab.daring.image.command.KBaseCommand
+import com.gitlab.daring.image.opencv.gray
+import com.gitlab.daring.image.opencv.toColored
+
+internal class CombineCommand(vararg args: String) : KBaseCommand(*args) {
+
+    val matKey = stringParam("")
+    val method = enumParam<CombineMethod>(null)
+
+    override fun execute(env: CommandEnv) {
+        var m1 = env.mat
+        var m2 = env.getMat(matKey.v)
+        if (m1.gray != m2.gray) {
+            m1 = m1.toColored()
+            m2 = m2.toColored()
+        }
+        method.v.func(m1, m2, m1)
+        env.mat = m1
+    }
+}
--- src/main/kotlin/com/gitlab/daring/image/opencv/OpencvExtension.kt
+++ src/main/kotlin/com/gitlab/daring/image/opencv/OpencvExtension.kt
@@ -2,11 +2,21 @@ package com.gitlab.daring.image.opencv
 
 import org.bytedeco.javacpp.opencv_core
 import org.bytedeco.javacpp.opencv_core.Mat
+import org.bytedeco.javacpp.opencv_imgproc.COLOR_GRAY2BGR
+import org.bytedeco.javacpp.opencv_imgproc.cvtColor
 
 val Mat.size get() = size()
 val Mat.width get() = cols()
 val Mat.height get() = rows()
+val Mat.type get() = type()
 val Mat.channels get() = channels()
 val Mat.empty get() = empty()
 val Mat.gray get() = channels == 1
+
 fun Mat.abs() = opencv_core.abs(this).asMat()
+
+fun Mat.toColored(): Mat {
+    val m = this
+    if (!gray) return m
+    return Mat().apply { cvtColor(m, this, COLOR_GRAY2BGR) }
+}
