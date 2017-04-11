14bbe7e97cc3c2121cc8aabcaf28b8834daf9232
daring <danila.daring@gmail.com>
2017-04-11 15:25:06 +0300

    issue #11 - refactoring

--- src/main/kotlin/com/gitlab/daring/image/command/transform/ConvertCommand.kt
+++ src/main/kotlin/com/gitlab/daring/image/command/transform/ConvertCommand.kt
@@ -2,22 +2,22 @@ package com.gitlab.daring.image.command.transform
 
 import com.gitlab.daring.image.command.CommandEnv
 import com.gitlab.daring.image.command.KBaseCommand
-import com.gitlab.daring.image.command.transform.ConvertCommand.Target.Grey
+import com.gitlab.daring.image.command.transform.ConvertCommand.Target.Gray
 import com.gitlab.daring.image.opencv.channels
 import org.bytedeco.javacpp.opencv_core.Mat
 import org.bytedeco.javacpp.opencv_imgproc.*
 
 internal class ConvertCommand(vararg params: String) : KBaseCommand(*params) {
 
-    val target = enumParam(Target.Grey)
+    val target = enumParam(Target.Gray)
 
     override fun execute(env: CommandEnv) {
-        if (env.mat.channels == 1 != (target.v == Grey)) {
-            val code = if (target.v == Grey) COLOR_BGR2GRAY else COLOR_GRAY2BGR
+        if (env.mat.channels == 1 != (target.v == Gray)) {
+            val code = if (target.v == Gray) COLOR_BGR2GRAY else COLOR_GRAY2BGR
             env.mat = Mat().apply { cvtColor(env.mat, this, code) }
         }
     }
 
-    enum class Target { Grey, Color }
+    enum class Target { Gray, Color }
 
 }
