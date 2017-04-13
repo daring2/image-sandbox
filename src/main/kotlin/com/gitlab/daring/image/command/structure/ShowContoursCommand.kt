9b3507f35b37e6b632cd86a994f74f232ce4a2c3
daring <danila.daring@gmail.com>
2017-04-13 19:25:18 +0300

    issue #11 - fix build

---
+++ src/main/kotlin/com/gitlab/daring/image/command/structure/ShowContoursCommand.kt
@@ -0,0 +1,24 @@
+package com.gitlab.daring.image.command.structure
+
+import com.gitlab.daring.image.command.CommandEnv
+import com.gitlab.daring.image.command.KBaseCommand
+import com.gitlab.daring.image.opencv.setTo
+
+internal class ShowContoursCommand(args: Array<String>) : KBaseCommand(args) {
+
+    val findCmd = FindContoursCommand()
+    val filterCmd = FilterContoursCommand(*args)
+    val drawCmd = DrawContoursCommand(emptyArray())
+
+    init {
+        params.addAll(filterCmd.params)
+    }
+
+    override fun execute(env: CommandEnv) {
+        findCmd.execute(env)
+        filterCmd.execute(env)
+        env.mat.setTo(0)
+        drawCmd.execute(env)
+    }
+
+}
